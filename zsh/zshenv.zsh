608b242fe75675f752d85b4b2f6fdb2e47ec3905
Matthew Price <hello@mattprice.me>
2019-07-03 22:13:59 -0400

    Cleanup zshenv

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,24 +1,23 @@
 # Do not go where the path may lead, go instead where there is
 # no path and leave a trail. — Ralph Waldo Emerson
-export PATH="/usr/local/bin:/usr/local/sbin:${PATH}"    # Homebrew
-export PATH="${HOME}/.nodenv/bin:${PATH}"               # nodenv
-export PATH="${HOME}/.rbenv/bin:${PATH}"                # rbenv
+export PATH="/usr/local/bin:/usr/local/sbin:${PATH}" # Homebrew
 
-# These are here so scripts/apps have access to Node and Ruby.
-# TODO: I don't think this works for apps anymore…
+# Node
+export PATH="${HOME}/.nodenv/bin:${PATH}"
 eval "$(nodenv init -)"
+
+# Ruby
+export PATH="${HOME}/.rbenv/bin:${PATH}"
 eval "$(rbenv init -)"
 
-# Set up the Golang environment
-if [[ $(uname -s) == 'Darwin' ]]; then
-   export GOPATH="${HOME}/Code/Go"
-   export GOBIN="${GOPATH}/bin"
-   export PATH="${GOBIN}:${PATH}"
-fi
+# Go
+export GOPATH="${HOME}/Code/Go"
+export GOBIN="${GOPATH}/bin"
+export PATH="${GOBIN}:${PATH}"
 
 # Google Cloud SDK
 if [[ -d "${HOME}/Code/google-cloud-sdk/" ]]; then
-    source "${HOME}/Code/google-cloud-sdk/path.zsh.inc"
+  source "${HOME}/Code/google-cloud-sdk/path.zsh.inc"
 fi
 
 # Disable global profiles, which overwrite our PATH
