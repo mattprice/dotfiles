8cbd3236d3ed0e02dfa19cf7c7953021e621c979
Matthew Price <hello@mattprice.me>
2020-05-06 09:00:17 +0200

    Enable subpixel antialiasing on macOS

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -8,4 +8,8 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 25
 # Enable Finder status bar (Finder -> View -> Show Status Bar)
 defaults write com.apple.finder ShowStatusBar -bool true
 
+# Enable subpixel antialiasing. Starting with Mojave, Apple disables it
+# by default, but that makes fonts look awful on non-Retina displays.
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
 echo "Please log out for settings to take effect."
