15821d723d0660299805bbd2a337bab06e6b5e25
Matthew Price <hello@mattprice.me>
2020-06-23 20:00:34 +0200

    Add PS Store to Finicky config

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -2,7 +2,7 @@ module.exports = {
   defaultBrowser: "Safari",
   handlers: [
     {
-      match: finicky.matchDomains("meet.google.com"),
+      match: finicky.matchDomains(["meet.google.com", "store.playstation.com"]),
       browser: "Google Chrome",
     },
   ],
