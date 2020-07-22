09bd99a770ea9cb93bf9c71ef1e1c39aaf6d32e4
Matthew Price <hello@mattprice.me>
2020-07-22 09:38:49 +0200

    Correct the matching scheme for Google Meet links

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -2,7 +2,14 @@ module.exports = {
   defaultBrowser: "Safari",
   handlers: [
     {
-      match: finicky.matchDomains(["meet.google.com", "store.playstation.com"]),
+      // PlayStation doesn't currently support Safari due to ITP.
+      match: finicky.matchDomains(["store.playstation.com"]),
+      browser: "Google Chrome",
+    },
+    {
+      // Fantastical opens URLs via a redirect (so the correct account is used).
+      // https://accounts.google.com/AccountChooser?Email=xxx&continue=https://meet.google.com/xxx
+      match: /meet\.google\.com/,
       browser: "Google Chrome",
     },
   ],
