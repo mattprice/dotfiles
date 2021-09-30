10da3fd05a7c20aceb374076a755baae2b49b7f2
Matthew Price <hello@mattprice.me>
2021-09-30 17:12:50 +0200

    Remove screen resolution automation

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,13 +8,3 @@ function printTable(table)
         print(k, v)
     end
 end
-
--- Automatically set the screen resolution to 1080p@1x when the TV is connected.
-function screenCallback(data)
-    screen = hs.screen("SAMSUNG")
-    if (screen) then
-        screen:setMode(1920, 1080, 1)
-    end
-end
-screenWatcher = hs.screen.watcher.new(screenCallback)
-screenWatcher:start()
