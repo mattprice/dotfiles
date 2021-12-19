ff0749a943e27de9c4f23ca8abed495045d4daa8
Matthew Price <hello@mattprice.me>
2021-12-19 13:02:41 -0500

    Open PlayStation in Safari instead of Chrome

--- finicky/finicky.js
+++ finicky/finicky.js
@@ -2,11 +2,6 @@ module.exports = {
   defaultBrowser: "Safari",
   handlers: [
     {
-      // PlayStation doesn't currently support Safari due to ITP.
-      match: finicky.matchDomains(["store.playstation.com"]),
-      browser: "Google Chrome",
-    },
-    {
       // Fantastical opens URLs via a redirect (so the correct account is used).
       // https://accounts.google.com/AccountChooser?Email=xxx&continue=https://meet.google.com/xxx
       match: /meet\.google\.com/,
