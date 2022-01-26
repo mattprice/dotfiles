e5dc0be4bf50ca6efac7ab0d58b286b04d7952d4
Matthew Price <hello@mattprice.me>
2022-01-26 16:32:03 +0100

    Add Fig support

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,8 @@
+#### FIG ENV VARIABLES ####
+# Please make sure this block is at the start of this file.
+[ -s ~/.fig/shell/pre.sh ] && source ~/.fig/shell/pre.sh
+#### END FIG ENV VARIABLES ####
+
 ### ZSH SETTINGS ###
 export EDITOR="code -w"
 export CLICOLOR=1
@@ -169,3 +174,8 @@ function wo() {
 if [[ -f "${HOME}/.zshrc_local" ]]; then
   source "${HOME}/.zshrc_local"
 fi
+
+#### FIG ENV VARIABLES ####
+# Please make sure this block is at the end of this file.
+[ -s ~/.fig/fig.sh ] && source ~/.fig/fig.sh
+#### END FIG ENV VARIABLES ####
