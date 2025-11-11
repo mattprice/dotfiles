8bf4aa6595f14f8002a3d3c039473892578c40c3
Matthew Price <hello@mattprice.me>
2025-11-11 18:06:16 +0100

    Migrate away from /usr/local/bin which no longer exists

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,8 +1,8 @@
 # Do not go where the path may lead, go instead where there is
 # no path and leave a trail. — Ralph Waldo Emerson
-export PATH="/usr/local/bin:/usr/local/sbin:${PATH}" # Homebrew x86
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}" # Homebrew ARM
-export PATH="$HOME/.local/share/mise/shims:$PATH" # Mise
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:${PATH}" # Homebrew
+export PATH="${HOME}/.local/share/mise/shims:${PATH}" # Mise
+export PATH="${HOME}/.local/bin:${PATH}"
 
 if [[ -f "${HOME}/.zshenv_local" ]]; then
   source "${HOME}/.zshenv_local"
