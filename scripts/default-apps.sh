d8cc05a0073fa3fd18b883874002e4d5288bf96a
Matthew Price <hello@mattprice.me>
2026-05-19 21:48:30 +0200

    Migrate default apps script to bash

---
+++ scripts/default-apps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script is based around a tool called Duti which is installed via Brew
+# https://github.com/moretension/duti
+
+if [[ $(uname -s) != 'Darwin' ]]; then
+  echo '^ Skipped because platform is not macOS.'
+  exit 0
+fi
+
+# Set default app for file extensions
+for ext in c cc css h js jsx json md php rb scss sh tf ts tsx txt xml yaml yml; do
+  duti -s com.microsoft.VSCode "$ext" all
+done
