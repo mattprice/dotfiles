e1b39f1d83e12db6af08464698185a922adf8441
Matthew Price <hello@mattprice.me>
2026-06-03 11:27:50 +0200

    Update comments to match new macOS location names

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -14,17 +14,17 @@ defaults write -g NSUserKeyEquivalents -dict-add "Duplicate" '@~$s' "Save As…"
 # Disable the language switcher popup
 defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled 0
 
-# Enable zoom using scroll gesture with Ctrl modifier (System Prefs -> Accessibility -> Zoom)
+# Enable zoom using scroll gesture with Ctrl modifier (System Settings -> Accessibility -> Zoom)
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess closeViewScrollWheelModifiersInt -int 262144
 # Use "continuously with pointer" panning mode
 defaults write com.apple.universalaccess closeViewPanningMode -int 0
 
-# Set key repeat speed (System Prefs -> Keyboard -> Keyboard)
+# Set key repeat speed (System Settings -> Keyboard -> Keyboard)
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
-# Set trackpad tracking speed (System Prefs -> Trackpad -> Point & Click)
+# Set trackpad tracking speed (System Settings -> Trackpad -> Point & Click)
 defaults write NSGlobalDomain com.apple.trackpad.scaling 0.875
 
 # Enable Finder status bar (View -> Show Status Bar)
@@ -39,21 +39,21 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # Dim the Dock icon for apps that are hidden
 defaults write com.apple.Dock showhidden -boolean yes
 
-# Don’t automatically rearrange Spaces based on most recent use (System Prefs -> Mission Control)
+# Don’t automatically rearrange Spaces based on most recent use (System Settings -> Desktop & Dock)
 defaults write com.apple.dock mru-spaces -bool false
 
-# Don’t show recent applications in Dock (System Prefs -> Dock)
+# Don’t show recent applications in Dock (System Settings -> Dock)
 defaults write com.apple.dock show-recents -bool false
 
-# Disable "Quick Note" hot corner (System Prefs -> Mission Control)
+# Disable "Quick Note" hot corner (System Settings -> Desktop & Dock -> Hot Corners)
 defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
 
-# Hide WiFi and Bluetooth from the menu bar (Control Center)
+# Hide WiFi and Bluetooth from the menu bar (System Settings -> Menu Bar)
 defaults -currentHost write com.apple.controlcenter WiFi -int 24
 defaults -currentHost write com.apple.controlcenter Bluetooth -int 24
 
-# Hide the date from the menu bar clock (System Prefs -> Control Center -> Clock Options)
+# Hide the date from the menu bar clock (System Settings -> Menu Bar -> Clock Options)
 defaults write com.apple.menuextra.clock ShowDate -int 2
 
 # Enable Safari's status bar (View -> Show Status Bar)
