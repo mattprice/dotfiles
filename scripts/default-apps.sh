7ee3b492d89ec26f67acadb4d62fc07085c3de15
Matthew Price <hello@mattprice.me>
2026-07-24 23:12:12 +0200

    Set Zed as the default editor

--- scripts/default-apps.sh
+++ scripts/default-apps.sh
@@ -10,5 +10,5 @@ fi
 
 # Set default app for file extensions
 for ext in c cc css h js jsx json md php rb scss sh tf ts tsx txt xml yaml yml; do
-  duti -s com.microsoft.VSCode "$ext" all
+  duti -s dev.zed.Zed "$ext" all
 done
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,4 +1,4 @@
-export EDITOR="code -w"
+export EDITOR="zed -w"
 export CLICOLOR=1
 export BAT_THEME="base16"
 export HOMEBREW_NO_ENV_HINTS=1
